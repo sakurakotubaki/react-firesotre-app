8a2e9a3726a817d8909c6f0e7df413cd7a7580bc
sakurakotubaki <gitonly543@gmail.com>
2024-02-17 15:37:21 +0900

    feat: create read delete

--- src/presentation/GetTodo.tsx
+++ src/presentation/GetTodo.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, memo } from "react";
-import { getTodos } from "../application/todo";
+import { getTodos, deleteTodo } from "../application/todo";
 import { Todo } from "../domain/Todo";
 
 const GetTodo = memo(() => {
@@ -16,12 +16,20 @@ const GetTodo = memo(() => {
     console.log("副作用が呼ばれた");
   }, []);// 第２引数が空の配列の場合、初回レンダリング時のみ副作用が呼ばれる
 
+  const handleDelete = async (todo: Todo) => {
+    await deleteTodo(todo);
+    setTodos(todos.filter(t => t.id !== todo.id));
+  };
+
   return (
     <div>
       <h1>Get Todo</h1>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
+          <li key={todo.id}>
+            {todo.title}
+            <button onClick={() => handleDelete(todo)}>Delete</button>
+          </li>
         ))}
       </ul>
     </div>
